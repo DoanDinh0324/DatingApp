acdab68ea28b64b8f60e96e517c4c4be31d624e1
DoanDinh0324 <110141767+DoanDinh0324@users.noreply.github.com>
2023-07-05 23:58:51 +0700

    End of section 4

---
+++ Extensions/ApplicationServicesExtesions.cs
@@ -0,0 +1,22 @@
+using API.Data;
+using API.Interfaes;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtesions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(otp =>
+                    {
+                         otp.UseSqlite(config.GetConnectionString(config["DefaultConnection"]));
+                    });
+
+                    return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaes;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,18 +15,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(otp =>
-{
-    otp.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
-builder.Services.AddCors();
 // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
@@ -28,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
